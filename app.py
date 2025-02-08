ab6455403231cf8124e9b7bfd5e31c3c824d4d83
chenx820 <chen.huang678@gmail.com>
2025-02-08 20:35:10 +0000

    Update

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from latex2html.latex2html import convert_latex_to_html
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/convert", methods=["POST"])
 def convert():
     if "file" not in request.files:
-        return jsonify({"error": "请上传 LaTeX 文件"}), 400
+        return jsonify({"error": "Please upload a LaTeX file"}), 400
     
     file = request.files["file"]
     format_type = request.form.get("format", "html")
@@ -17,7 +19,7 @@ def convert():
         if format_type == "html":
             converted_content = convert_latex_to_html(latex_content)
         else:
-            converted_content = "功能还没写完！"
+            converted_content = "Coming soon!"
 
         return jsonify({"result": converted_content})
     except Exception as e:
